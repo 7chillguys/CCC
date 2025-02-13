ebe1101013cffb9fc5c5981f8c82ed1bf173d662
Kimseungbeen <129667842+Kimseungbeen@users.noreply.github.com>
2025-02-13 14:06:34 +0900

    build : 폴더 수정

---
+++ ccc-chat/src/main/java/com/example/cccchat/CccChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.cccchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CccChatApplication.class, args);
+    }
+
+}
---
+++ ccc-chat/src/test/java/com/example/cccchat/CccChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cccchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
